361f4ac6d9bb574b649a73f94d8de29a6a57cb00
jongdukwon <77368612+jongdukwon@users.noreply.github.com>
2021-02-14 00:09:30 +0900

    Update Reservation.java

--- reservation/src/main/java/restaurant/Reservation.java
+++ reservation/src/main/java/restaurant/Reservation.java
@@ -43,7 +43,7 @@ public class Reservation {
     @PrePersist
     public void onPrePersist(){ 
         try {
-            Thread.currentThread().sleep((long) (600 + Math.random() * 220));
+            Thread.currentThread().sleep((long) (1000 + Math.random() * 220));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
